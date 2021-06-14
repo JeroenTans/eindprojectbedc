ef2c055901963cd8ad00615a35a93f08d3311d60
JeroenTans <jeroentans1@gmail.com>
2021-06-14 15:16:50 +0200

    review controller ruw

--- src/main/java/com/example/eindprojectbedc/controller/ReviewController.java
+++ src/main/java/com/example/eindprojectbedc/controller/ReviewController.java
@@ -1,17 +1,47 @@
 package com.example.eindprojectbedc.controller;
 
-import com.example.eindprojectbedc.Service.ReviewServiceImp;
+import com.example.eindprojectbedc.Service.ReviewService;
+import com.example.eindprojectbedc.controller.dto.ReviewDto;
+import com.example.eindprojectbedc.controller.dto.ReviewInputDto;
 import com.example.eindprojectbedc.model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
 
+    private final ReviewService reviewService;
 
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping
+    public List<ReviewDto> getAllReviews() {
+        var dtos = new ArrayList<ReviewDto>();
+        var allReviews = reviewService.getAllReviews();
+
+        for (Review review : allReviews) {
+            dtos.add(ReviewDto.fromReview(review));
+        }
+        return dtos;
+    }
+
+    @GetMapping("/{id}")
+    public ReviewDto getReview(@PathVariable("id") Long id) {
+        var review = reviewService.getReview(id);
+        return ReviewDto.fromReview(review);
+    }
+
+    @PostMapping
+    public ReviewDto saveReview(@RequestBody ReviewInputDto dto) {
+        var review = reviewService.saveReview(dto.toReview());
+        return ReviewDto.fromReview(review);
+    }
 }
