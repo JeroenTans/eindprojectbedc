1c3b5334a3a7b99b13d7642f4c0bafde46e5494c
JeroenTans <jeroentans1@gmail.com>
2021-06-17 14:18:13 +0200

    Upload af

--- src/main/java/com/example/eindprojectbedc/config/SpringSecurityConfig.java
+++ src/main/java/com/example/eindprojectbedc/config/SpringSecurityConfig.java
@@ -4,20 +4,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    public CustomUserDetailsService customUserDetailsService;
+//    @Autowired
+//    private JwtRequestFilter jwtRequestFilter;
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(customUserDetailsService);
+//    }
+
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //JWT token authentication
+        http
+                .csrf().disable()
+                .authorizeRequests()
+//                .antMatchers("/api/v1/authenticate").permitAll()
+//                .antMatchers(HttpMethod.GET,"/api/v1/naam/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET,"/api/v1/users/**/authorities").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
     @Configuration
     public class WebConfig {
-
         @Autowired
         private CrosFilter corsFilter;
-
         @Bean
         public FilterRegistrationBean corsFilter() {
             FilterRegistrationBean registration = new FilterRegistrationBean();
@@ -28,5 +69,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             return registration;
         }
     }
-
 }
